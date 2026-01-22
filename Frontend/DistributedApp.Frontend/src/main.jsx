f8cfbd3fb0331bf0ddc1aa37cd2fe65708ce8afe
maicol_jb <64624079+maik151@users.noreply.github.com>
2026-01-21 20:35:00 -0500

    fix> corregir main.jsx

--- Frontend/DistributedApp.Frontend/src/main.jsx
+++ Frontend/DistributedApp.Frontend/src/main.jsx
@@ -4,10 +4,18 @@ import App from './App.jsx'
 import './index.css' 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+// 1. IMPORTA TU PROVEEDOR (Ajusta la ruta si tu archivo está en otro lado)
+import { AuthProvider } from './features/auth/context/AuthContext'; 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="220912218741-c0t2luqp91uhshf0j9bc8ojblu7f9hfh.apps.googleusercontent.com">
-      <App />
+      
+      {/* 2. ENVUELVE TU APP CON EL AUTHPROVIDER */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+
     </GoogleOAuthProvider>
   </React.StrictMode>,
 )
